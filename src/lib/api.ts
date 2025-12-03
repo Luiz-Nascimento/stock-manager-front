19c3e9b442028d8f440360c08ef2c9c0dc10e302
Luiz Nascimento <luiznascimento.developer@gmail.com>
2025-12-03 17:59:46 -0300

    chore: changed url api again

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Garanta que esta é a porta onde seu back-end Spring Boot está rodando
 const api = axios.create({
-  baseURL: 'https://stock-manager-a9uj.onrender.com'
+  baseURL: 'https://stock-manager-luiz-c903789022f1.herokuapp.com'
 });
 
 export default api;
