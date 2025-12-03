05f8466fa5d56071f5cae27388f9ee9709de292c
Luiz Nascimento <luiznascimento.developer@gmail.com>
2025-12-03 17:37:42 -0300

    fix: changed api url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Garanta que esta é a porta onde seu back-end Spring Boot está rodando
 const api = axios.create({
-  baseURL: 'https://app-stockmanager-api.graybay-87632aed.eastus.azurecontainerapps.io'
+  baseURL: 'https://stock-manager-a9uj.onrender.com'
 });
 
 export default api;
